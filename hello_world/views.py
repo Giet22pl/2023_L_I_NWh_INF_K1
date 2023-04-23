8f4bffbdfc79749d36fccf93951859b5ebc987a4
Kamil_Rogala <kamill2224r@gmail.com>
2023-04-23 23:08:45 +0200

    Poprawka

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,13 +4,13 @@ from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 moje_imie = "Kamil"
 msg = "Hello World!"
-@app.route('/')
+@app.route('/') # noqa
 def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
-@app.route('/outputs')
+@app.route('/outputs') # noqa
 def supported_output():
-    return ", ".join(SUPPORTED)
+    return ", ".join(SUPPORTED) # noqa
